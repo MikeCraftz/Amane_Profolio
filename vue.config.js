34fbf5ac60858b04b005e07ac2081fe7b1c7a93b
YoyoyoMic <80812251+MicCKW@users.noreply.github.com>
2024-11-06 20:20:32 +0900

    updating conifg

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  base: './'
 })
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Amane_Profolio/' : '/'
-};
