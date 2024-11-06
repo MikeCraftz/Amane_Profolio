d2781ad43e0091dd44d4eb6bd897c1be4bd45ef4
YoyoyoMic <80812251+MicCKW@users.noreply.github.com>
2024-11-06 20:21:25 +0900

    update vite config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()],
   base: './'
-})
+});
