c58e5dab15937daaa0c2e60a69162aa0c588f6d2
YoyoyoMic <80812251+MicCKW@users.noreply.github.com>
2024-11-06 20:34:48 +0900

    fixing index

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../components/HomePage.vue';
 import GalleryPage from '../components/GalleryPage.vue';
 
